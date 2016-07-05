48575074e934d10fe2f001bf8710c0e5dbb11e49
Yuhang <abeyuhang@gmail.com>
2016-07-05 21:23:57 +0800

    update

--- index.js
+++ index.js
@@ -1,21 +1,73 @@
 'use strict';
 
-const SilenceJS = require('silence-js');
-const BaseLogger = SilenceJS.BaseLogger;
+const format = require('util').format;
 
-class ConsoleLogger extends BaseLogger {
-  constructor(config) {
-    super(config);
-    this._resolve('ready');
+const LEVELS = {
+  NONE: 4,
+  ERROR: 3,
+  WARN: 2,
+  INFO: 1,
+  DEBUG: 0
+};
+const TIPS = ['DEBUG', 'INFO', 'WARN', 'ERROR', 'NONE'];
+
+function formatDate(date) {
+  function fn(n) {
+    return n < 10 ? '0' + n : n.toString()
   }
-  _write(level, section, args) {
-    if (args.length > 1 && _.isString(args[0])) {
-      console.log(TIPS[level] + ':', util.format.apply(null, args));
-    } else if (_.isString(args[0])) {
-      console.log(TIPS[level] + ':', args[0]);
-    } else {
-      console.log.apply(console, args);
+  function fm(n) {
+    return n < 10 ? '00' + n : (n < 100 ? '0' + n : n.toString());
+  }
+  date = date ? date : new Date();
+  return `${date.getFullYear()}/${fn(date.getMonth()+1)}/${fn(date.getDate())} ${fn(date.getHours())}:${fn(date.getMinutes())}:${fn(date.getSeconds())}.${fm(date.getMilliseconds())}`;
+}
+
+const consoleFnMap = {
+  [LEVELS.INFO]: console.log,
+  [LEVELS.WARN]: console.warn,
+  [LEVELS.ERROR]: console.error,
+  [LEVELS.DEBUG]: console.log
+}
+class ConsoleLogger {
+  constructor(config) {
+    this.level = LEVELS[(config.level || 'ERROR').toUpperCase()];
+  }
+  init() {
+    return Promise.resolve();
+  }
+  close() {
+    return Promise.resolve();
+  }
+  _log(level, ...args) {
+    if (level < this.level) {
+      return;
     }
+    this._write(level, ...args);
+  }
+  _format(level, ...args) {
+    let prefix = `[${formatDate()}] [${TIPS[level]}] `;
+    return prefix + format(...args);
+  }
+  log(...args) {
+    this._log(LEVELS.INFO, ...args);
+  }
+  debug(...args) {
+    this._log(LEVELS.DEBUG, ...args);
+  }
+  error(...args) {
+    this._log(LEVELS.ERROR, ...args);
+  }
+  info(...args) {
+    this._log(LEVELS.INFO, ...args);
+  }
+  warn(...args) {
+    this._log(LEVELS.WARN, ...args);
+  }
+  _write(level, ...args) {
+    if (args.length === 0) {
+      return;
+    }
+    consoleFnMap[level].call(console, this._format(level, ...args));
   }
 }
 
