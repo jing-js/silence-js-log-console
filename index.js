b335817645938fe7614ea47abf1f1a6652390e93
Yuhang <abeyuhang@gmail.com>
2016-08-31 14:48:19 +0800

    update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const format = require('util').format;
+const cluster = require('cluster');
 
 const LEVELS = {
   NONE: 4,
@@ -11,15 +12,29 @@ const LEVELS = {
 };
 const TIPS = ['[DEBUG]', '[INFO ]', '[WARN ]', '[ERROR]', '[NONE ]'];
 
+// 不清楚琐碎的数字到字符串格式化对性能是否有影响。反正先生成好放那儿, 也占不了多少内存。
+const PAD_2_NUMS = (new Array(100).fill(0).map((n, i) => pad(i)));
+const PAD_3_NUMS = (new Array(1000).fill(0).map((n, i) => pad3(i)));
+const TIME_ZONE = (function () {
+  let to = (new Date()).getTimezoneOffset();
+  let tn = to < 0;
+  to = to < 0 ? -to : to;
+  let th = (to / 60) | 0;
+  let tm = (to - th * 60);
+  return ` GMT${tn ? '+' : '-'}${pad(th)}${pad(tm)}`
+})();
+
+function pad(n) {
+  return n < 10 ? '0' + n : n.toString();
+}
+
+function pad3(n) {
+  return n < 10 ? '00' + n : (n < 100 ? '0' + n : n.toString());
+}
+
 function formatDate(date) {
-  function fn(n) {
-    return n < 10 ? '0' + n : n.toString()
-  }
-  function fm(n) {
-    return n < 10 ? '00' + n : (n < 100 ? '0' + n : n.toString());
-  }
   date = date ? date : new Date();
-  return `${date.getFullYear()}/${fn(date.getMonth()+1)}/${fn(date.getDate())} ${fn(date.getHours())}:${fn(date.getMinutes())}:${fn(date.getSeconds())}.${fm(date.getMilliseconds())}`;
+  return `${date.getFullYear()}/${PAD_2_NUMS[date.getMonth()+1]}/${PAD_2_NUMS[date.getDate()]} ${PAD_2_NUMS[date.getHours()]}:${PAD_2_NUMS[date.getMinutes()]}:${PAD_2_NUMS[date.getSeconds()]}.${PAD_3_NUMS[date.getMilliseconds()]}${TIME_ZONE}`;
 }
 
 const consoleFnMap = {
@@ -45,7 +60,7 @@ class ConsoleLogger {
     this._write(level, ...args);
   }
   _format(level, ...args) {
-    let prefix = `[${formatDate()}] ${TIPS[level]} `;
+    let prefix = (cluster.isWorker ? `[${cluster.worker.id}] ` : '') + `[${formatDate()}] ${TIPS[level]} `;
     return prefix + format(...args);
   }
   log(...args) {
@@ -68,13 +83,13 @@ class ConsoleLogger {
       return;
     }
     if (typeof args[0] !== 'string') {
-      consoleFnMap[level].call(console, `[${formatDate()}] [${TIPS[level]}]`, ...args); 
+      consoleFnMap[level].call(console, (cluster.isWorker ? `[${cluster.worker.id}] ` : '') + `[${formatDate()}] ${TIPS[level]}`, ...args);
     } else {
       consoleFnMap[level].call(console, this._format(level, ...args));      
     }
   }
   access(method, code, duration, ip, url) {
-    console.log(`[${formatDate()}] [${code !== 0 && code < 1000 ? code : 200}] [${method}] [${duration}ms] [${ip}] ${url}\n`);
+    console.log((cluster.isWorker ? `[${cluster.worker.id}] ` : '') + `[${formatDate()}] [${code !== 0 && code < 1000 ? code : 200}] [${method}] [${duration}ms] [${ip}] ${url}\n`);
   }
 }
 
