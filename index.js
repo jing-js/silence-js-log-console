f1ee8af35254de079542ab9ea9f2964f5b1198eb
Yuhang <abeyuhang@gmail.com>
2016-09-30 11:03:20 +0800

    update

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ const util = require('silence-js-util');
 const cluster = require('cluster');
 
 const LEVELS = {
-  NONE: 4,
+  NONE: 5,
+  ACCESS: 4,
   ERROR: 3,
   WARN: 2,
   INFO: 1,
   DEBUG: 0
 };
-const TIPS = ['[DEBUG]', '[INFO ]', '[WARN ]', '[ERROR]', '[NONE ]'];
+const TIPS = ['[DEBUG]', '[INFO ]', '[WARN ]', '[ERROR]', '[ACCES]'];
 
 const consoleFnMap = {
   [LEVELS.INFO]: console.log,
@@ -29,47 +30,59 @@ class ConsoleLogger {
   close() {
     return Promise.resolve();
   }
-  _log(level, ...args) {
+  _log(level, section, ...args) {
     if (level < this.level) {
       return;
     }
-    this._write(level, ...args);
+    this._write(level, section.toUpperCase(), ...args);
   }
-  _format(level, ...args) {
-    let prefix = (cluster.isWorker ? `[${cluster.worker.id}] ` : '') + `[${util.formatDate()}] ${TIPS[level]} `;
+  _format(level, section, ...args) {
+    let prefix = (cluster.isWorker ? `[${cluster.worker.id}] ` : '') + `[${util.formatDate()}] ${TIPS[level]} [${section}] `;
     return prefix + format(...args);
   }
-  log(...args) {
-    this._log(LEVELS.INFO, ...args);
-  }
   debug(...args) {
-    this._log(LEVELS.DEBUG, ...args);
+    this._log(LEVELS.DEBUG, 'all', ...args);
   }
   error(...args) {
-    this._log(LEVELS.ERROR, ...args);
+    this._log(LEVELS.ERROR, 'all', ...args);
   }
   info(...args) {
-    this._log(LEVELS.INFO, ...args);
+    this._log(LEVELS.INFO, 'all', ...args);
   }
   warn(...args) {
-    this._log(LEVELS.WARN, ...args);
+    this._log(LEVELS.WARN, 'all', ...args);
   }
-  _write(level, ...args) {
+  sdebug(section, ...args) {
+    this._log(LEVELS.DEBUG, section, ...args)
+  }
+  sinfo(section, ...args) {
+    this._log(LEVELS.INFO, section, ...args)
+  }
+  serror(section, ...args) {
+    this._log(LEVELS.ERROR, section, ...args)
+  }
+  swarn(section, ...args) {
+    this._log(LEVELS.WARN, section, ...args)
+  }
+  _write(level, section, ...args) {
     if (args.length === 0) {
       return;
     }
     if (typeof args[0] !== 'string') {
-      consoleFnMap[level].call(console, (cluster.isWorker ? `[${cluster.worker.id}] ` : '') + `[${util.formatDate()}] ${TIPS[level]}`, ...args);
+      consoleFnMap[level].call(console, (cluster.isWorker ? `[${cluster.worker.id}] ` : '') + `[${util.formatDate()}] ${TIPS[level]} [${section}] `, ...args);
     } else {
-      consoleFnMap[level].call(console, this._format(level, ...args));      
+      consoleFnMap[level].call(console, this._format(level, section, ...args));      
     }
   }
   access(method, code, duration, bytesRead, bytesWritten, user, ip, userAgent, url) {
+    if (this.level === LEVELS.NONE) {
+      return;
+    }
     let ds = duration < 2000 ? duration + 'ms' : (duration / 1000 | 0) + 's';
     if (userAgent && userAgent.indexOf('"') >= 0) {
       userAgent = userAgent.replace(/\"/g, '\\"')
     }
-    console.log((cluster.isWorker ? `[${cluster.worker.id}] ` : '') + `[${util.formatDate()}] [${code !== 0 && code < 1000 ? code : 200}] [${method}] [${ds}] [${bytesRead}] [${bytesWritten}] [${user ? user : '-'}] [${ip}] "${userAgent || ''}" ${url}`);
+    console.log((cluster.isWorker ? `[${cluster.worker.id}] ` : '') + `[${util.formatDate()}] ${TIPS[LEVELS.ACCESS]} [${code !== 0 && code < 1000 ? code : 200}] [${method}] [${ds}] [${bytesRead}] [${bytesWritten}] [${user ? user : '-'}] [${ip}] "${userAgent || ''}" ${url}`);
   }
 }
 
